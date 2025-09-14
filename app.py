ece881642ff8129ceb4f770c09b1b52b2f309fce
Trayz72 <shahtanishq72@gmail.com>
2025-09-14 17:21:34 +0530

    Changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Automated Software Testing!"
+    return "Hello, Automated Testing!"
 
 @app.route("/add/<int:a>/<int:b>")
 def add(a, b):
