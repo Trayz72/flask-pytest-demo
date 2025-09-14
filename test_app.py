8fcf8a28cd0db71d4eb2f97ec4351b3da07d74d7
Trayz72 <shahtanishq72@gmail.com>
2025-09-14 10:08:54 +0530

    Initial commit - Flask app with PyTest

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.data == b"Hello, Automated Testing!"
+
+def test_add(client):
+    response = client.get("/add/2/3")
+    assert response.data == b"5"
